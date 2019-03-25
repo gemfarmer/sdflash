17b4765db450b4664b01b69186cd9db60c2d9a7f
Brian Hedberg <bshslowfood@gmail.com>
2019-03-25 17:19:53 -0500

    starting to work on read

--- recipedetails.ino
+++ recipedetails.ino
@@ -20,20 +20,21 @@ void setup() {
 void loop() {
   SDCardReader.poll();
   delay(3000);
-  // SDCardStatus.getRecipes = true;
+  SDCardStatus.getRecipes = true;
   // SDCardStatus.getFirmware = true;
-  SDCardStatus.isWriting = true;
+  SDCardStatus.isReading = true;
+  // SDCardStatus.isWriting = true;
 
-  randomSeed(analogRead(A0)); //Use the analog pins for a good seed value
-  int randomNumber = random(999); //Select a random file #, 0 to 999
-  if (randomNumber > 500) {
-    SDCardStatus.moreToWrite = true;
-    char output[3]; //Max file name length is "12345678.123" (12 characters)
+  // randomSeed(analogRead(A0)); //Use the analog pins for a good seed value
+  // int randomNumber = random(999); //Select a random file #, 0 to 999
+  // if (randomNumber > 500) {
+  //   SDCardStatus.moreToWrite = true;
+  //   char output[3]; //Max file name length is "12345678.123" (12 characters)
 
-    sprintf(output, "%03d", randomNumber);
-    Serial.printlnf("output=%s",output);
-    SDCardStatus.dataToLog = output;
-  } else {
-    Serial.println("No random match");
-  }
+  //   sprintf(output, "%03d", randomNumber);
+  //   Serial.printlnf("output=%s",output);
+  //   SDCardStatus.dataToLog = output;
+  // } else {
+  //   Serial.println("No random match");
+  // }
 }
